cabb0689242356cc8fd3f6a119f329d16d6bebc7
Michał Sakwa <saka1988@gmail.com>
2015-12-08 23:52:01 +0100

    Added first use of last.fm api.

--- js/spolastify.js
+++ js/spolastify.js
@@ -1,5 +1,14 @@
 var app = angular.module('spolastify', []);
 
-app.controller('MainCtrl', function($scope) {
+app.controller('MainCtrl',['$scope', '$http', function($scope, $http) {
     $scope.name = 'spolastify';
-});
+    $scope.trackList = [];
+    $scope.loadingData = true;
+    $scope.apiKey= '6a82616e5f43a1ab1cf7cede2b547e8a';
+    $http.get('http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&format=json&user=saka2&api_key=6a82616e5f43a1ab1cf7cede2b547e8a&period=12month')
+        .success(function(data) {
+            $scope.trackList = data.toptracks.track;
+    }).finally(function() {
+      $scope.loadingData = false;  
+    });
+}]);
